c8def512a0034854123e603e9dde4b3a237aa4e2
tamirakian <tamirakian@gmail.com>
2024-06-05 23:55:45 +0300

    Part 2 completed - LRU cache implementation

---
+++ Models/CacheItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TinyURLApp.Models;
+
+public class CacheItem<TKey, TValue>
+{
+    public TKey Key { get; }
+    public TValue Value { get; }
+
+    public CacheItem(TKey key, TValue value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
---
+++ Services/ICacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace TinyURLApp.Services;
+
+public interface ICacheService<TKey, TValue>
+{
+    TValue? Get(TKey key);
+    void Add(TKey key, TValue value);
+    void Clear();
+}
